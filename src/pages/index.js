ff796d5c767c5df9101ad8be33848e68b4c095f0
dondevo <daudomar10@gmail.com>
2019-06-07 19:32:18 -0400

    seo stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Daud Omar" keywords={[`Daud Omar`, `Daud`, `Software Developer`]} />
     <LandingBio />
   </Layout>
 )
