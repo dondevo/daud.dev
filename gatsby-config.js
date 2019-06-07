1d56009837816c2bd2fddfbcff8ee192b72e79a5
dondevo <daudomar10@gmail.com>
2019-06-06 23:24:25 -0400

    fix build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-      {
+  
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-140668879-2",
